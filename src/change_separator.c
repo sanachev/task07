985148751aa150aeb3a3061a4772dc50e729a2a9
Stanislav Sanachev <stas@MacBook-Pro-Stanislav.local>
2024-03-22 00:19:49 +0300

    Level 2 finished

---
+++ src/change_separator.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int step;
+    int count = 1;
+    char* data;
+    char separator[2];
+    data = (char*)malloc(sizeof(char) * count);
+
+    // читаем строку
+    while (1) {
+        step = getchar();
+        if (step == 10) {
+            break;
+        } else {
+            count++;
+            data = (char*)realloc(data, sizeof(char) * count);
+            data[count - 2] = step;
+        }
+    }
+
+    // читаем разделитель
+    fgets(separator, 2, stdin);
+
+    // преобразуем пробелы в строке
+    for (int i = 0; i < count - 1; i++) {
+        if (data[i] == 32) {
+            data[i] = separator[0];
+        }
+    }
+
+    // выводим результат
+    for (int i = 0; i < count - 1; i++) {
+        printf("%c", data[i]);
+    }
+
+    free(data);
+    return 0;
+}
