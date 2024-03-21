5f5a7b4cade4c547125b073e5536b1190200cc20
Stanislav Sanachev <stas@MacBook-Pro-Stanislav.local>
2024-03-21 22:32:25 +0300

    Read all fields from db is OK

--- src/show_str.c
+++ src/show_str.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 #include <sqlite3.h>
 
+
+int callback(void*, int, char**, char**);
+
 int main(void){
     sqlite3 *db;
-    int result = sqlite3_open("../data-samples/tasck007.db", &db);
-    printf("%d\n", result);
+    char *err_msg = 0;
+
+    sqlite3_open("../data-samples/task007.db", &db);
+
+    char *sql = "SELECT * FROM Students";
+
+    sqlite3_exec(db, sql, callback, 0, &err_msg);
+
     sqlite3_close(db);
 
+    return 0;
+}
+
+int callback(void *notUsed, int colCount, char **columns, char **colNames)
+{
+    for (int i = 0; i < colCount; i++)
+    {
+        printf("%s = %s\n", colNames[i], columns[i] ? columns[i] : "NULL");
+    }
+    printf("\n");
     return 0;
 }
